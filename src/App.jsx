db49e7419b52fc36552a885c4b21e4f76992b293
2k23csds2310790-creator <2k23.csds2310790@gmail.com>
2025-12-01 16:15:52 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+export default function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      margin: 0,
+      backgroundColor: '#ffffff'
+    }}>
+      <h1 style={{
+        fontSize: '200px',
+        margin: 0,
+        fontWeight: 'bold',
+        color: '#000000'
+      }}>
+        HELLO!
+      </h1>
+    </div>
+  )
+}
